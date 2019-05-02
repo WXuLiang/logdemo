ceebc5eb9ce08acfdf201b654f0c094d29c6b305
wxl <31200198@qq.com>
2019-05-02 18:58:07 +0800

    00

--- src/main/java/com/example/logdemo/LogdemoApplication.java
+++ src/main/java/com/example/logdemo/LogdemoApplication.java
@@ -1,26 +1,13 @@
 package com.example.logdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class LogdemoApplication extends SpringBootServletInitializer {
+public class LogdemoApplication {
 
     //springboot war 包要继承LogdemoApplication 并 重写config方法
 
     public static void main(String[] args) {
-        SpringApplication.run(LogdemoApplication.class, args);
+        System.out.println("nihao");
+        ;
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        System.out.println("dd");
-        return builder.sources(LogdemoApplication.class);
-
-
-
-    }
 
 }
